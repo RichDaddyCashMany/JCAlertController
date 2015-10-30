a8be8ac9717036e7898f479d6fc38b6634cc5ce7
hjaycee <hjaycee@163.com>
2015-10-30 19:08:44 +0800

    change

---
+++ JCAlertView-master/JCAlertView-master/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JCAlertView-master
+//
+//  Created by mac on 15/10/30.
+//  Copyright © 2015年 HJaycee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JCAlertView-master/JCAlertView-masterTests/JCAlertView_masterTests.m
@@ -0,0 +1,39 @@
+//
+//  JCAlertView_masterTests.m
+//  JCAlertView-masterTests
+//
+//  Created by mac on 15/10/30.
+//  Copyright © 2015年 HJaycee. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JCAlertView_masterTests : XCTestCase
+
+@end
+
+@implementation JCAlertView_masterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
